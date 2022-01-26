fca69c183ee3c27636e5575eef6d7251612220d5
kapka videnova <videnovakapka@gmail.com>
2022-01-26 16:44:21 +0200

    Add exercises

---
+++ CSarp-WebBasics/Routing/BasicWebServer/BasicWebServer/Startup.cs
@@ -0,0 +1,15 @@
+﻿using BasicWebServer.Server.Responses;
+using BasiWebServer.Server;
+
+namespace BasicWebServer.Demo
+{
+    public class Startup
+    {
+        public static void Main()
+            => new HttpServer(routes => routes
+            .MapGet("/",new TextResponse("Hello from the server."))
+            .MapGet("/HTML",new HtmlResponse("<h1>HTML response</h1"))
+            .MapGet("/Redirect", new RedirectResponse("https://softuni.org/")))       
+            .Start();      
+    }
+}
