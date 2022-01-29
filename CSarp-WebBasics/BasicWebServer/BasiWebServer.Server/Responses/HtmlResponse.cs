7b7cd608c5633fddd941cc5605b9ceb7034d3631
kapka videnova <videnovakapka@gmail.com>
2022-01-29 10:05:51 +0200

    Basic server

---
+++ CSarp-WebBasics/BasicWebServer/BasiWebServer.Server/Responses/HtmlResponse.cs
@@ -0,0 +1,14 @@
+﻿using BasicWebServer.Server.HTTP;
+using System;
+
+namespace BasicWebServer.Server.Responses
+{
+    public class HtmlResponse : ContentResponse
+    {
+        public HtmlResponse(string text,
+            Action<Request,Response> preRenderAction = null)
+            :base(text, ContentType.Html,preRenderAction)
+        {
+        }
+    }
+}
